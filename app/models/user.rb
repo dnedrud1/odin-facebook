267c0ba59040f39a099ed07dd8245e4b345ad3e3
dnedrud1 <dnedrud1@gmail.com>
2015-04-27 17:21:17 +0000

    Added names to User model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ActiveRecord::Base
+  # attr_accessible :name
+  
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
@@ -8,4 +10,5 @@ class User < ActiveRecord::Base
   has_many :comments, dependent: :destroy
   has_many :likes, dependent: :destroy
   
+  validates :name, presence: true, length: { maximum: 50 }
 end
