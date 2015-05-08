15f2b1ecb31067f1130c61a296ad2dd20f7c9cef
dnedrud1 <dnedrud1@gmail.com>
2015-05-08 05:59:44 +0000

    heroku test 4

--- app/models/user.rb
+++ app/models/user.rb
@@ -27,7 +27,7 @@ class User < ActiveRecord::Base
   validates :name, presence: true, length: { maximum: 50 }
   
   def self.from_omniauth(auth)
-    where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
+    where(email: auth.info.email).first_or_create do |user|
       # user.email = auth.info.email
       user.password = Devise.friendly_token[0,20]
       user.name = auth.info.name
