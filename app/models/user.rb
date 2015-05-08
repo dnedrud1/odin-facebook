dd912d6177e784ab465d44210b69530815838f1a
dnedrud1 <dnedrud1@gmail.com>
2015-05-08 06:01:34 +0000

    heroku test 5

--- app/models/user.rb
+++ app/models/user.rb
@@ -28,6 +28,8 @@ class User < ActiveRecord::Base
   
   def self.from_omniauth(auth)
     where(email: auth.info.email).first_or_create do |user|
+      user.provider   = auth.provider
+      user.uid        = auth.uid
       # user.email = auth.info.email
       user.password = Devise.friendly_token[0,20]
       user.name = auth.info.name
